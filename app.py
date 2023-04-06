bd540b9a33b8c856d040ae878a83251c32c7fd40
chankruze <chankruze@gmail.com>
2023-04-06 21:26:10 +0530

    hide loading spinner for data_cache
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import streamlit as st
 
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def create_df(file: str):
     return pd.read_json(file)
 
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
@@ -53,24 +53,32 @@ if __name__ == '__main__':
     # check if the user uploaded a file
     if uploaded_file is not None:
         # convert json to a dataframe
-        dataframe = create_df(uploaded_file)
-        # show the dataframe in the app
-        st.write(dataframe)
-        # convert the dataframe to csv
-        csv = convert_df(dataframe)
-        # show the download button in the app to download the csv file
-        st.download_button(
-            label="Download as CSV",
-            data=csv,
-            file_name=generate_file_name(),
-            mime='text/csv',
-        )
+        with st.spinner("Converting JSON to CSV..."):
+            dataframe = create_df(uploaded_file)
 
-        st.write("#### 📝 Privacy & Security")
-        st.write("1. The file is cached for 10 minutes.")
-        st.write("2. The file is deleted after 10 minutes.")
-        st.write("3. The file is not stored anywhere.")
-        st.write("4. The file is not shared with anyone.")
-        st.write("5. The file is not used for any other purpose.")
-        st.write("")
-        st.write("Made with ❤️ by [chankruze](https://github.com/chankruze)")
+            st.write("#### 📊 Data Preview")
+            # show the dataframe in the app
+            st.write(dataframe)
+            # convert the dataframe to csv
+            csv = convert_df(dataframe)
+
+            st.write("#### 📥 Download")
+            # show the download button in the app to download the csv file
+            st.download_button(
+                label="Download as CSV",
+                data=csv,
+                file_name=generate_file_name(),
+                mime='text/csv',
+            )
+
+            st.write("#### 📝 Privacy & Security")
+            st.write("1. The file is cached for 10 minutes.")
+            st.write("2. The file is deleted after 10 minutes.")
+            st.write("3. The file is not stored anywhere.")
+            st.write("4. The file is not shared with anyone.")
+            st.write("5. The file is not used for any other purpose.")
+            st.write("")
+            st.write("Made with ❤️ by [chankruze](https://github.com/chankruze)")
+
+            # drop snow on the app
+            st.snow()
